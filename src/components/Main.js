885c7f49784bb846936556665cee9e8ad341e90b
liutaochange <liutaochange@163.com>
2018-02-02 23:57:35 +0800

    init

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,18 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//获取图片相关的数据
+var imgData = require('../data/imgdata.json');
+var imgdatas = (function (imgDataarr){
+  imgDataarr.forEach(element => {
+    element.singleUrl = require('../images/'+element.file_name)
+  });
+  return imgDataarr;
+})(imgData)
+
+//
 
 class AppComponent extends React.Component {
   render() {
