49179b156f2958084f9b64fe0b245d36d63fc39e
liutaochange <liutaochange@163.com>
2018-02-03 19:46:15 +0800

    project replenish

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.scss');
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 //获取图片相关的数据
 var imgData = require('../data/imgdata.json');
@@ -25,13 +26,56 @@ class ImgFigure extends React.Component{
 
 //页面整体结构
 class AppComponent extends React.Component {
+  Constant (){
+    return {
+      center:{
+        left: 0,
+        top:0
+      },
+      hPosRange:{
+        leftSecX:[0,0],
+        rightSecX:[0,0],
+        y:[0,0]
+      },
+      vPosRange:{
+        x:[0,0],
+        topY:[0,0]
+      }
+    }
+  }
+  componentDidMount() {
+    //获取舞台的大小
+    var stageEle = ReactDOM.findDOMNode(this.refs.stage);
+    var stageW = stageEle.scrollWidth;
+    var stageH = stageEle.scrollHeight;
+    var halfStageW = Math.round(stageW/2);
+    var halfStageH = Math.round(stageH/2);
+
+    //获取图片的大小
+
+    var imgEle = ReactDOM.findDOMNode(this.refs.imgfig0);
+    var imgW = imgEle.scrollWidth;
+    var imgH = imgEle.scrollHeight;
+    var halfImgW = Math.round(imgW/2);
+    var halfImgH = Math.round(imgH/2);
+
+    //计算区域 中心点
+    this.Constant.center = {
+      left: halfStageW-halfImgW,
+      top: halfStageH-halfImgH
+    }
+
+    this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+    this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW*3;
+    this.Constant.hPosRange.rightSecX[0] = halfStageW - halfImgW*3;
+  }
   render() {
     var controllerUnits = [],ImgFigures = [];
-    imgData.forEach(element => {
-      ImgFigures.push(<ImgFigure data={element}/>);
+    imgData.forEach((element,index) => {
+      ImgFigures.push(<ImgFigure data={element} ref={'imgfig'+index} key={index}/>);
     });
     return (
-     <section className="stage">
+     <section className="stage" ref='stage'>
       <section className="img-sec">
         {ImgFigures}
       </section>
