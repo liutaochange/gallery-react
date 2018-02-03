60ccdf8ed73d2b426e09fb8ed8d7d8c0e6dde166
liutaochange <liutaochange@163.com>
2018-02-03 18:13:47 +0800

    start project

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,23 +5,40 @@ import React from 'react';
 
 //获取图片相关的数据
 var imgData = require('../data/imgdata.json');
-var imgdatas = (function (imgDataarr){
-  imgDataarr.forEach(element => {
-    element.singleUrl = require('../images/'+element.file_name)
-  });
-  return imgDataarr;
-})(imgData)
+imgData.forEach(element => {
+  element.singleUrl = require('../images/'+element.file_name)
+});
 
-//
+//img模块
+class ImgFigure extends React.Component{
+  render(){
+    return(
+      <figure className="img-wamp">
+        <img src={this.props.data.singleUrl} alt={this.props.data.title} className="img-item"/>
+        <figcaption>
+          <h3 className="img-title">{this.props.data.title}</h3>
+        </figcaption>
+      </figure>
+    )
+  }
+}
 
+//页面整体结构
 class AppComponent extends React.Component {
   render() {
+    var controllerUnits = [],ImgFigures = [];
+    imgData.forEach(element => {
+      ImgFigures.push(<ImgFigure data={element}/>);
+    });
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">hello react</div>
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+     <section className="stage">
+      <section className="img-sec">
+        {ImgFigures}
+      </section>
+      <nav className="controller-mav">
+        {controllerUnits}
+      </nav>
+     </section>
     );
   }
 }
